48ced320ec0e3c6b05f1145b455b21f462e5420e
michel <kemtamich@yahoo.fr>
2017-01-10 08:50:08 +0100

    Imagesnapper fertig

--- ImageSnapper/main.cpp
+++ ImageSnapper/main.cpp
@@ -3,31 +3,8 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-#ifdef WIN32
-#include <windows.h>
-#elif _POSIX_C_SOURCE >= 199309L
-#include <time.h>   // for nanosleep
-#else
-#include <unistd.h> // for usleep
-#endif
-
 using namespace std;
 using namespace cv;
-
-void sleep_ms(int milliseconds) // cross-platform sleep function
-{
-#ifdef WIN32
-    Sleep(milliseconds);
-#elif _POSIX_C_SOURCE >= 199309L
-    struct timespec ts;
-    ts.tv_sec = milliseconds / 1000;
-    ts.tv_nsec = (milliseconds % 1000) * 1000000;
-    nanosleep(&ts, NULL);
-#else
-    usleep(milliseconds * 1000);
-#endif
-}
-
 
 int main()
 {
