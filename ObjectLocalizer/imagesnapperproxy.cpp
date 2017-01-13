35cd4dbb28f2b7a14f77a3d7dd7d072cd4455c8f
Kai <you@example.com>
2017-01-13 02:14:43 +0100

    erstintegration

--- ObjectLocalizer/imagesnapperproxy.cpp
+++ ObjectLocalizer/imagesnapperproxy.cpp
@@ -2,5 +2,20 @@
 
 ImageSnapperProxy::ImageSnapperProxy()
 {
+    this->setImagePath("C:\\SP1\\scene.bmp");
+}
 
+void ImageSnapperProxy::setImagePath(string path)
+{
+    this->scenePath = path;
+}
+
+string ImageSnapperProxy::getImagePath()
+{
+    return this->scenePath;
+}
+
+cv::Mat ImageSnapperProxy::getImage()
+{
+    return cv::imread(this->getImagePath());
 }
