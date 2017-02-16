fd4a0491bd17f139c7fcfd24e664fcd0a5701236
Kai <you@example.com>
2017-02-16 14:38:24 +0100

    einige Kommentare entfernt

--- ObjectLocalizer/objectpickerproxy.cpp
+++ ObjectLocalizer/objectpickerproxy.cpp
@@ -8,7 +8,6 @@ void ObjectPickerProxy::pick(PositionT pos)
 {
     float center[2];
     //Transformation in cm
-    //center = pos.getCenter();
     center[0] = pos.getCenter()[0] * 0.00694; // 18cm/1333px = 0.0135
     center[1] = pos.getCenter()[1] * 0.00699; // 13,6cm/1000px = 0.0136
 
@@ -29,7 +28,7 @@ void ObjectPickerProxy::pick(PositionT pos)
 
     float grad = 0;
 
-    grad = atan2(center[0],center[1]) * (180/3.141);
+    grad = pos.getOrientation();
 
     ss << grad;
 
@@ -38,18 +37,10 @@ void ObjectPickerProxy::pick(PositionT pos)
 
     //Konvertieren in JSON
     string json;
-    /*
-    json_spirit::Object jsonDump;
-
-    jsonDump.push_back( json_spirit::Pair("Distanz",dist));
-    jsonDump.push_back( json_spirit::Pair("Polarwinkel",polar));
-    jsonDump.push_back( json_spirit::Pair("Rotationswinkel",rot));
-
-    */
 
     json = "{\"Distanz\":" + diststr + ",\"PolarWinkel\":" + gradstr + "}";
     cout << json << endl;
 
-    //MQTTNode::sendMsg(json);
+    MQTTNode::sendMsg(json);
 
 }
